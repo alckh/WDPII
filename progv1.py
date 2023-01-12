0e6eb208ded17e4638d9312b0a938655ce8b0233
ortcs <120735394+ortcs@users.noreply.github.com>
2023-01-12 15:10:22 +0100

    mapV1_orlane

---
+++ progv1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 15:03:16 2023
+
+@author: orlan
+"""
+
+import folium 
+map = folium.Map(location= [48.8514,2.2886], zoom_start=20)
+map.save('maCarte1.html')
