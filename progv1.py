a7addac2d2268be92313afffb8fb662bc648ccc8
ortcs <120735394+ortcs@users.noreply.github.com>
2023-01-19 15:08:11 +0100

    Load json

--- progv1.py
+++ progv1.py
@@ -8,6 +8,18 @@ Created on Thu Jan 12 15:03:16 2023
 import folium 
 from folium import plugins
 
+import json
+
+
+x = '[{"latitude":43.41800914565575,"longitude":-1.4736806863206051,"town":"Ustaritz","postcode":"64480","epci":"CA du Pays Basque","county":"Pyrénées-Atlantiques","images":["https://api.v2.clean2gether.com/uploads/c2g_upload_ee31b829ca.jpg"]},{"latitude":43.42567782128301,"longitude":-1.4851003904940752,"town":"Arcangues","postcode":"64200","epci":"CA du Pays Basque","county":"Pyrénées-Atlantiques","images":["https://api.v2.clean2gether.com/uploads/c2g_upload_4a96cf89ac.jpg"]}]'
+
+y = json.loads(x)
+
+print(y[0]["latitude"])
+print(y[1]["latitude"])
+
+
+
 data =[[    45.6754,    0.24273,   6000.    ],
        [    45.59092,    0.27279,   5200.    ],
        [    45.54825,    0.12207,   5800.    ],
