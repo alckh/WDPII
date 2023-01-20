54a60fe4441725c637080205645aa4326452e573
ortcs <120735394+ortcs@users.noreply.github.com>
2023-01-20 09:27:49 +0100

    Updade fichier csv Angouleme

--- progv1.py
+++ progv1.py
@@ -12,12 +12,12 @@ from folium.plugins import HeatMap
 import pandas as pd
 
 df = pd.read_csv('angouleme.csv',encoding='latin-1')
-df.drop(df[(df['town']!="AngoulÇ¦me")].index, inplace=True)
+df.drop(df[(df['town']!="Angouleme")].index, inplace=True)
 df.reset_index(inplace = True, drop = True)
 
 dataset= pd.DataFrame({'latitude':df['latitude'],'longitude':df['longitude'],'intensite':6000})
 
-m = folium.Map([45.6754, 0.24273],control_scale = True, zoom_start=11)
+m = folium.Map([45.64454, 0.14273],control_scale = True, zoom_start=13)
 
 heat_data = [[row['latitude'],row['longitude']] for index, row in dataset.iterrows()]
 HeatMap(heat_data, radius = 20, min_opacity = 0.9,gradient={.6: 'yellow', .98: 'orange', 1: 'red'}).add_to(m)
