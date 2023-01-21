af8c9bc561304ba74e9ac53b99c784d00fa43ac9
Celiancaf <104433371+Celiancaf@users.noreply.github.com>
2023-01-21 15:34:41 +0100

    Ajout de la fonction Volume_Toxicite
    Génération d'une liste aléatoire de volume et de toxicité des déchets

--- progv1.py
+++ progv1.py
@@ -10,6 +10,7 @@ import numpy as np
 from folium import plugins
 from folium.plugins import HeatMap
 import pandas as pd
+import random
 
 df = pd.read_csv('angouleme_segmenté.csv',encoding='latin-1')
 
@@ -22,3 +23,20 @@ HeatMap(heat_data, radius = 20, min_opacity = 0.9,gradient={.6: 'yellow', .98: '
 
 m.save('maCarte1.html')
 
+
+def Volume_Toxicite(K): #Avec K le nombre de dates pour un même dépôt sauvage
+    L_volume=[]
+    L_toxicite=[]
+    for i in range(0,K):
+        v = random.randint(0,3) #Echelle du volume de déchet: 0/Il y a zéro déchet 1/Sac poubelle (ex:30L) 2/Brouette (ex:100L) 3/Camion
+        L_volume.append(v)
+       
+        t = random.randint(1,4) #Echelle de la toxicité: 0/Non toxique pour l'environnement 1/Faiblement toxique 2/Toxique 3/Très toxique 4/Extrêmement toxique
+        if v==0: #S'il n'y a pas de déchet la toxicité est de 0
+            L_toxicite.append(0)
+        else:
+            L_toxicite.append(t)
+            
+    return L_volume, L_toxicite
+
+"""print(Volume_Toxicite(5))"""
